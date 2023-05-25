d74896c5119b5fd36e5be5b83b00effc0b7175f0
Zahra Al Bulushi <132909001+zahra998@users.noreply.github.com>
2023-05-25 13:56:03 +0400

    advance indexer

---
+++ Book/Book/FavBook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    public class FavBook
+    {
+        public string BookName { get; set; }
+        public string Authere { get; set; }
+     
+        public string bookInfo;
+
+
+        public string this[int index, string bookName, string authere] {
+
+            set{
+                this.BookName = value;
+                this.Authere = value;
+            }
+            get{
+
+                this.BookName = bookName;
+                this.Authere = authere;
+
+               
+
+                bookInfo = $"book name: {BookName}, authore: {Authere}";
+                return bookInfo;
+            }
+        }       
+    }
+}
---
+++ Book/Book/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FavBook favbok = new FavBook();
+
+           
+
+            Console.WriteLine(favbok[0, "C#", "Joe"]);
+            Console.WriteLine(favbok[1, "C++", "Jhon"]);
+        }
+    }
+}
